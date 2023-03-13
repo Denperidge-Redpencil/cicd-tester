e7e3c83d966407d8580f4acba1f14de83ddb6edd
Denperidge <denperidge@gmail.com>
2023-03-13 13:28:27 +0100

    index.py now generates a file

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from requests import request
 
-print(request("https://github.com/"))
+with open("index.py.txt", "w") as file:
+    file.write(str(request("https://github.com/")))
 print("Succesful python run!")
