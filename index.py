e99053889baa279af9c2731b11e57620a243967e
Denperidge <denperidge@gmail.com>
2023-03-13 13:37:49 +0100

    Updated incorrect request syntax

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from requests import request
 
 with open("index.py.txt", "w") as file:
-    file.write(str(request("https://github.com/")))
+    file.write(str(request(method="GET", url="https://github.com/")))
 print("Succesful python run!")
